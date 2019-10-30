433678f008f12b9f50fbcb33a157de830b9aa9e1
LorenzoCeleghin <56909314+LorenzoCeleghin@users.noreply.github.com>
2019-10-30 08:08:02 +0100

    Update Triangolo.java

--- Triangolo/src/triangolo/Triangolo.java
+++ Triangolo/src/triangolo/Triangolo.java
@@ -6,12 +6,9 @@ public class Triangolo {
    
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Inserire un numero tra 0 e 15");
+        System.out.println("Inserire un numero");
         System.out.println("N = ");
         int n = sc.nextInt();
-        if (n<0||n>15) {
-            System.out.println("Numero non valido");
-        }
         int a = 1;
         int b = 0;
         int c = 1;
